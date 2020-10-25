a0f3d0a39384cafd9989cb9c4ec3c6acd9917109
Kyle Linzy <kllinzy1@protonmail.com>
2020-10-25 13:51:31 -0500

    added some packages

--- init.el
+++ init.el
@@ -9,7 +9,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t)
- '(package-selected-packages (quote (web-mode csharp-mode evil))))
+ '(package-selected-packages
+   (quote
+    (ivy-clipmenu swiper counsel use-package web-mode csharp-mode evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
